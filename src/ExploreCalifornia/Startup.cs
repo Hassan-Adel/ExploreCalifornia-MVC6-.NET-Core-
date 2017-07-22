f2988c1e37c492a382aab9e19ecf2a84cca4ae4c
hassan.adel <hasanadel555@hotmail.com>
2017-07-23 00:12:50 +0200

    Increase maintainablility with dependency injection

--- src/ExploreCalifornia/Startup.cs
+++ src/ExploreCalifornia/Startup.cs
@@ -13,29 +13,44 @@ namespace ExploreCalifornia
 {
     public class Startup
     {
+        private readonly IConfigurationRoot configuration;
+
+        public Startup(IHostingEnvironment env) {
+            configuration = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddJsonFile(env.ContentRootPath + "/config.json")
+                .AddJsonFile(env.ContentRootPath + "/config.development.json", true)
+                .Build();
+        }
+
+        // Our IOC Container (Inversion Of Control)
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            /*
+             ASP.NET Core allows you to completely control how instances are created by allowing you to pass the AddTransient
+             method a function, that returns an instance of type. For example, 
+             I can create my own instance of the FeatureToggles class, like this.
+             */
+            services.AddTransient<FeatureToggles>(x => new FeatureToggles{
+                EnableDeveloperExceptions = configuration.GetValue<bool>("FeatureToggles:EnableDeveloperExceptions")
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, FeatureToggles feature)
         {
             loggerFactory.AddConsole();
 
             // telling ASP.NET Core's configuration library about the various places that the application's configuration settings will be stored. 
             // true for optional
-            var configuration = new ConfigurationBuilder()
-                .AddEnvironmentVariables()
-                .AddJsonFile(env.ContentRootPath + "/config.json")
-                .AddJsonFile(env.ContentRootPath + "/config.development.json", true)
-                .Build();
+            
 
             app.UseExceptionHandler("/error.html");
 
             //use the configuration object just like a dictionary.
-            if (configuration.GetValue<bool>("FeatureToggles:EnableDeveloperExceptions"))
+            if (feature.EnableDeveloperExceptions)
             {
                 app.UseDeveloperExceptionPage();
             }
