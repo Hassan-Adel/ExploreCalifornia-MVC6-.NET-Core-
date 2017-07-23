154f387af315805d9a3c35cee367306683ab1a99
hassan.adel <hasanadel555@hotmail.com>
2017-07-23 04:09:31 +0200

    Pass data using ViewBag

--- src/ExploreCalifornia/Controllers/BlogController.cs
+++ src/ExploreCalifornia/Controllers/BlogController.cs
@@ -27,11 +27,11 @@ namespace ExploreCalifornia.Controllers
         [Route("blog/{year:min(2000)}/{month:range(1,12)}/{key}")]
         public IActionResult Post(int year, int month, string key)
         {
-            return new ContentResult
-            {
-                Content = string.Format("Year: {0};  Month: {1};  Key: {2}",
-                                        year, month, key)
-            };
+            ViewBag.Title = "";
+            ViewBag.Posted = DateTime.Now;
+            ViewBag.Author = "Hassan Adel";
+            ViewBag.Body = "My First post on this site !!";
+            return View();
         }
     }
 }
