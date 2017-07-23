52755f08e2d69549db65cbc2efa69cfb1f5b4c4c
hassan.adel <hasanadel555@hotmail.com>
2017-07-24 01:51:39 +0200

    Creating a new post

--- src/ExploreCalifornia/Controllers/BlogController.cs
+++ src/ExploreCalifornia/Controllers/BlogController.cs
@@ -61,5 +61,22 @@ namespace ExploreCalifornia.Controllers
             };
             return View(post);
         }
+
+        // GET: /<blog>/create
+        [HttpGet, Route("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // Post: /<blog>/create
+        [HttpPost, Route("create")]
+        public IActionResult Create(Post post)
+        {
+            post.Author = User.Identity.Name;
+            post.Posted = DateTime.Now;
+            return View();
+        }
+
     }
 }
