692d1d6f0a2477792364c3fe9d52ed7614faeea6
hassan.adel <hasanadel555@hotmail.com>
2017-07-24 02:03:29 +0200

    Adding validation to post model

--- src/ExploreCalifornia/Models/Post.cs
+++ src/ExploreCalifornia/Models/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,15 @@ namespace ExploreCalifornia.Models
 {
     public class Post
     {
+        [Required]
+        [Display(Name = "Post Title")]
+        [DataType(DataType.Text)]
+        [StringLength(100, MinimumLength = 5, ErrorMessage ="Title must be between 5 and 100")]
         public string Title { get; set; }
         public string Author { get; set; }
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [MinLength(100, ErrorMessage = "Blog Posts must be atleat 100 characters long")]
         public string Body { get; set; }
         public DateTime Posted { get; set; }
     }
