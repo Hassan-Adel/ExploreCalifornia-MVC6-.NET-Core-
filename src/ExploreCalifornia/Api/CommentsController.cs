b93627aab0b1199e77c678348331de2bd5aa48bb
hassan.adel <hasanadel555@hotmail.com>
2017-07-24 23:34:24 +0200

    Expost Comment data through the web API

--- src/ExploreCalifornia/Api/CommentsController.cs
+++ src/ExploreCalifornia/Api/CommentsController.cs
@@ -3,19 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ExploreCalifornia.Models;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ExploreCalifornia.Api
 {
-    [Route("api/[controller]")]
+    [Route("api/post/{postKey}/comments")]
     public class CommentsController : Controller
     {
+        private readonly BlogDataContext _db;
+
+        public CommentsController(BlogDataContext db)
+        {
+            _db = db;
+        }
+
         // GET: api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IQueryable<Comment> Get(string postKey)
         {
-            return new string[] { "value1", "value2" };
+            return _db.Comments.Where(comment => comment.Post.Key == postKey);
         }
 
         // GET api/values/5
@@ -25,10 +33,21 @@ namespace ExploreCalifornia.Api
             return "value";
         }
 
+        //Visual Studio generated also prefixed the comment parameter with the FromBody attribute. 
+        //This attribute tells model binding to only consider the body of the Http request when it's attempting to populate the properties of the parameter.
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public Comment Post(string postKey, [FromBody]Comment comment)
         {
+            var Post = _db.Posts.Where(post => post.Key == postKey).FirstOrDefault();
+            if (Post == null)
+                return null;
+
+            comment.Post = Post;
+            comment.Posted = DateTime.Now;
+            _db.Add(comment);
+            _db.SaveChanges();
+            return comment;
         }
 
         // PUT api/values/5
