fc67ec0db3f2e1fc31234a5e6fc39e1a7f2d654f
hassan.adel <hasanadel555@hotmail.com>
2017-07-25 01:50:49 +0200

    Adding Login

--- src/ExploreCalifornia/Controllers/AccountController.cs
+++ src/ExploreCalifornia/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using ExploreCalifornia.Models.Account;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -10,13 +11,67 @@ namespace ExploreCalifornia.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly SignInManager<IdentityUser> _signinManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AccountController(
+                UserManager<IdentityUser> userManager,
+                SignInManager<IdentityUser> signinManager
+            )
+        {
+            _userManager = userManager;
+            _signinManager = signinManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View(new LoginViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel login, string returnUrl = null)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var result = await _signinManager.PasswordSignInAsync(
+                login.EmailAddress, login.Password,
+                login.RememberMe, false
+            );
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Login error!");
+                return View();
+            }
+
+            if (string.IsNullOrWhiteSpace(returnUrl))
+                return RedirectToAction("Index", "Home");
+
+            return Redirect(returnUrl);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout(string returnUrl = null)
+        {
+            await _signinManager.SignOutAsync();
+
+            if (string.IsNullOrWhiteSpace(returnUrl))
+                return RedirectToAction("Index", "Home");
+
+            return Redirect(returnUrl);
+        }
+
+
         public IActionResult Register()
         {
             return View(new RegisterViewModel());
         }
 
         [HttpPost]
-        public IActionResult Register(RegisterViewModel registration)
+        public async Task<IActionResult> Register(RegisterViewModel registration)
         {
             if (!ModelState.IsValid)
                 return View(registration);
@@ -27,7 +82,19 @@ namespace ExploreCalifornia.Controllers
                 UserName = registration.EmailAddress,
             };
 
-            // TODO:  Finish registering the user
+            var result = await _userManager.CreateAsync(newUser, registration.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors.Select(x => x.Description))
+                {
+                    ModelState.AddModelError("", error);
+                }
+
+                return View();
+            }
+
+            return RedirectToAction("Login");
         }
     }
 }
